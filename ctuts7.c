fb53a8a916236f0902aefd6b302ad3bf6fc240b3
Himanshu Gupta <himanshugupta930521@gmail.com>
2026-08-18 20:14:43 +0530

    Create ctuts7.c

---
+++ ctuts7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+  
+    int a, b;
+    a = 76;
+    b = 89;
+  
+    printf("a || b = %d\n", a&b);
+    printf("a - b = %d\n", a-b);
+    printf("a * b = %d\n", a*b);
+    printf("a / b = %d\n", a/b);
+    
+    return 0;
+}
