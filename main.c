b974e80a1cb3424ecd2e89af5354ad50f4714895
Himanshu Gupta <himanshugupta930521@gmail.com>
2026-08-18 20:05:43 +0530

    Add main function to print a sequence of messages

---
+++ main.c
@@ -0,0 +1,290 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main() {
+
+    printf("Jamin ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("se ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("asman ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("tak ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hmm...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("dhundh ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("aaye ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("jahan ");
+    fflush(stdout);
+    Sleep(7400);
+
+    printf("sara...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("bana ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("paya ");
+    fflush(stdout);
+    Sleep(7400);
+
+    printf("nahi ");
+    fflush(stdout);
+    Sleep(7400);
+
+    printf("ab ");
+    fflush(stdout);
+    Sleep(7400);
+
+    printf("takk...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("khuda ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("tumshe ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("koe ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("pyara...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("Baton ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("me ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("teri ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hai ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("badmashiyan...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("sabb ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("bee ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("bajah ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("kee ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hai ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("tarifiyan...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("mai ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("likh ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("dun ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("asman ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("par ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("yee...\n");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("kee ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("padh ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("lega ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("jahan ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("shara...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("hua ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("na ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hoga ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("abb ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("koe...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("yahan ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hmm ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("doo ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("sa ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("dobara...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("mai ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("duniya ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("bhar ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("kee ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("tarifen...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("tere ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("shajde ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("mee ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("laya ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hun...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    printf("mai ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("tumshe ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("ishaq ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("karne ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("kee ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("izazat ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("rabb ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("see ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("laya ");
+    fflush(stdout);
+    Sleep(400);
+
+    printf("hun...\n");
+    fflush(stdout);
+    Sleep(900);
+
+    return 0;
+}
+    
+    
+
